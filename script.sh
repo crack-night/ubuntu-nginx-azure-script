9d73b0578906ee31dd135d81cef641994bd548f4
crack::night <leomarqz.main@gmail.com>
2026-05-11 23:48:21 -0600

    Update HTML content and include server ID

--- script.sh
+++ script.sh
@@ -3,22 +3,28 @@ apt-get update -y
 apt-get install nginx -y
 systemctl enable nginx
 systemctl start nginx
+
+# Generar el ID simple
+MY_ID=$(cat /proc/sys/kernel/random/uuid)
+
 cat <<EOF > /var/www/html/index.html
 <!DOCTYPE html>
 <html>
 <head>
-<title>Azure Web Server</title>
+<title>Server ID</title>
 <style>
 body { font-family: sans-serif; background: #2c3e50; color: white; text-align: center; }
 .box { border: 2px solid #27ae60; display: inline-block; padding: 20px; margin-top: 50px; }
+.uuid { color: #f1c40f; font-weight: bold; }
 </style>
 </head>
 <body>
 <div class="box">
-<h1>Deployment Successful</h1>
-<p>Nginx is running on Ubuntu in Azure.</p>
+<h1>Servidor Activo</h1>
+<p>ID de Instancia: <span class="uuid">$MY_ID</span></p>
 </div>
 </body>
 </html>
 EOF
+
 systemctl restart nginx
